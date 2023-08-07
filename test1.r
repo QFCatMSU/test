43a8fa1080b0ac7e2290e730da0015b726f8578f
Charlie <belinsky@msu.edu>
2023-08-07 14:06:47 -0400

    Second test

---
+++ test1.r
@@ -0,0 +1,16 @@
+{
+  a <- 2;
+  b <- 3
+
+  adder = function(a, b) 
+    {
+    return(a +b)
+  }
+
+  cat(adder(a,b));
+}
+
+
+## To use: Addins -> Style active file
+##
+## Or run this command: styler:::style_active_file()
