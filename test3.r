88997c930834994fc40628fca62368df9a06832b
Charlie <belinsky@msu.edu>
2024-01-30 12:38:02 -0500

    delteions

--- test3.r
+++
@@ -1,2 +0,0 @@
-sddsafgfaAmnstuff
-# more stuffsafsadasdf
--- test4.r
+++
@@ -1,7 +0,0 @@
-{
-  {#kjlhlo
-    a 
-## To use: Addins -> Style active file
-##
-## Or run this command: styler:::style_active_file()
-stuff changed
