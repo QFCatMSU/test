7408518927881d80e94bc6bba92e890384b50bee
Charlie <belinsky@msu.edu>
2024-02-06 17:45:41 -0500

    wefrwfr

--- test1.r
+++ test1.r
@@ -15,8 +15,4 @@
 ## To use: Addins -> Style active file
 ## Change this
 ## Or run this command: styler:::style_active_file()
-<<<<<<< HEAD
 ## add stuff 
-=======
-## add stuff stuff
->>>>>>> 290e0f3e111d2255abd11bcec8ee010118c633a7
