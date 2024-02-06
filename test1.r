6485d83c9b15f76b1c4b960877a464e2c510eb1a
Charlie <belinsky@msu.edu>
2024-02-06 17:42:01 -0500

    will this merge?

--- test1.r
+++ test1.r
@@ -1,7 +1,7 @@
 {
   a <- 12;
   b <- 3;
-  c = a*b+b; #blah
+  c = a*b+b; 
 
   adder = function(a, b) 
     {
@@ -15,4 +15,4 @@
 ## To use: Addins -> Style active file
 ## Change here
 ## Or run this command: styler:::style_active_file()
-## add stuff
+## add stuff stuff
